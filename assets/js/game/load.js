8d981505a2c9509fafa644758effdc332a8f82d3
KyleMartin95 <kylemartin95@gmail.com>
2017-04-12 13:28:08 -0400

    rearranged and added comments to logic.js to make more sense

--- assets/js/game/load.js
+++ assets/js/game/load.js
@@ -1,109 +1,54 @@
 var game = null;
-var context = null;
-var frameRate = 1000/10;
-var frame = 0;
-var direction;
-var floors = [];
-var frames = [];
-var background = null;
-var girl = null;
-var hitMessage = new Image();
-var arrows = [];
 
-$(document).ready(function(){
+function Game(){
+  this.element = document.getElementById("game");
+  this.context = this.element.getContext("2d");
+  this.element.width = 1275;
+  this.element.height = 1080;
+  this.frameRate = 1000/10;
+  this.frame = 0;
+  this.direction;
+  this.floors = [];
+  this.frames = [];
 
-  var setup = function(){
-    game = document.getElementById("game");
-    context = game.getContext("2d");
-    game.width = 1275;
-    game.height = 1080;
-    loadBackground();
-    loadFloors();
-    loadGirl();
-    loadArrows();
-    setInterval(animate, frameRate);
-  };
+  this.background = new Image();
+  this.background.onload = onImageLoad;
+  this.background.src = '/images/transparent_background.png';
 
-  setup();
-});
-
-var loadBackground = function(){
-  background = new Image();
-  background.onload = onImageLoad;
-  background.src = '/images/transparent_background.png';
-}
-
-var loadFloors = function(){
   for(var i = 0; i <= 11; i++){
     var floorSource = '/images/dance_floor' + i + '.png';
-    floors.push(floorSource);
+    this.floors.push(floorSource);
   }
-  for(var i = 0; i <= floors.length; i++){
-    frames.push(new Image());
-    frames[i].onload = onImageLoad;
-    frames[i].src = floors[i];
+  for(var i = 0; i <= this.floors.length; i++){
+    this.frames.push(new Image());
+    this.frames[i].onload = onImageLoad;
+    this.frames[i].src = this.floors[i];
   }
-};
 
-var loadGirl = function(){
-  girl = new Image();
-  girl.onload = onImageLoad;
-  girl.src = '/images/chibi-girl.png';
-}
+  this.girl = new Image();
+  this.girl.onload = onImageLoad;
+  this.girl.src = '/images/chibi-girl.png';
 
-var loadArrows = function(){
+  this.hitMessage = new Image();
+
+  this.arrows = [];
   var arrowSRC = ['/images/up_left_arrow.png', '/images/up_arrow.png', '/images/up_right_arrow.png',
             '/images/right_arrow.png', '/images/down_right_arrow.png', '/images/down_arrow.png',
             '/images/down_left_arrow.png', '/images/left_arrow.png'];
   for(var i = 0; i <= arrowSRC.length; i++){
-    arrows.push(new Image());
-    arrows[i].onload = onImageLoad;
-    arrows[i].src = arrowSRC[i];
+    this.arrows.push(new Image());
+    this.arrows[i].onload = onImageLoad;
+    this.arrows[i].src = arrowSRC[i];
   }
-}
 
-var animate = function(){
-  context.clearRect(0, 0, game.width, game.height);
-  context.drawImage(frames[frame], 0, 595);
-  context.drawImage(background, 0, 0);
-  context.drawImage(girl, 530, 250);
-  context.drawImage(arrows[0], 50, 10);
-  context.drawImage(arrows[1], 530, 10);
-  context.drawImage(arrows[2], 1060, 10);
-  context.drawImage(arrows[7], 50, 400);
-  context.drawImage(arrows[3], 1060, 400);
-  context.drawImage(arrows[6], 50, 700);
-  context.drawImage(arrows[5], 530, 700);
-  context.drawImage(arrows[4], 1060, 700);
-  //decides where to put hit message if there is one
-  if(hitMessage.src != '#'){
-    switch(direction){
-      case 'up_left':
-        context.drawImage(hitMessage, 50, 10);
-        break;
-      case 'up_right':
-        context.drawImage(hitMessage, 1060, 10);
-        break;
-      case 'left':
-        context.drawImage(hitMessage, 50, 400);
-        break;
-      case 'right':
-        context.drawImage(hitMessage, 1060, 400);
-        break;
-      case 'back_left':
-        context.drawImage(hitMessage, 50, 700);
-        break;
-      case 'back_right':
-        context.drawImage(hitMessage, 1060, 700);
-        break;
-      default:
-        //do nothing
-        break;
-    }
-  }
-  frame = ((frame + 1) % floors.length);
-};
+  setInterval(animate, this.frameRate);
+}
 
 var onImageLoad = function(){
   console.log("loaded image");
 };
+
+//start the game
+$(document).ready(function(){
+  game = new Game();
+});
--- assets/js/game/logic.js
+++ assets/js/game/logic.js
@@ -1,45 +1,35 @@
-$(document).ready(function(){
-  $(document).keypress(function(event){
-    keyPressEvent(event);
-  });
-});
-
 var keyMap = {
-  'q': 'up_left',
-  'w': 'up',
-  'e': 'up_right',
-  'a': 'left',
-  'd': 'right',
-  'z': 'back_left',
-  'x': 'down',
-  'c': 'back_right'
+  'q': 'up_left', 'w': 'up', 'e': 'up_right', 'a': 'left', 'd': 'right',
+  'z': 'back_left', 'x': 'down', 'c': 'back_right'
 };
 
 var keyPressEvent = function(event){
   var key = event.key;
   switch(key){
     case 'q':
-      direction = keyMap.q;
+      //set direction girl points to
+      game.direction = keyMap.q;
+      //calls hit event to make animation happen
       hit(keyMap.q)
       break;
     case 'e':
-      direction = keyMap.e;
+      game.direction = keyMap.e;
       hit(keyMap.e)
       break;
     case 'a':
-      direction = keyMap.a;
+      game.direction = keyMap.a;
       hit(keyMap.a)
       break;
     case 'd':
-      direction = keyMap.d;
+      game.direction = keyMap.d;
       hit(keyMap.d)
       break;
     case 'z':
-      direction = keyMap.z;
+      game.direction = keyMap.z;
       hit(keyMap.z)
       break;
     case 'c':
-      direction = keyMap.c;
+      game.direction = keyMap.c;
       hit(keyMap.c)
       break;
     default:
@@ -54,19 +44,69 @@ var hit = function(direction){
   //in case of multiple hits before reset
   clearTimeout(reset);
 
-  girl = new Image();
-  girl.onload = onImageLoad;
-  girl.src = '/images/girl_' + direction + '.png';
-  hitMessage.onload = onImageLoad;
-  hitMessage.src = '/images/hit.png';
+  game.girl = new Image();
+  game.girl.onload = onImageLoad;
+  game.girl.src = '/images/girl_' + direction + '.png';
+  game.hitMessage.onload = onImageLoad;
+  game.hitMessage.src = '/images/hit.png';
   animate();
 
   //resets the girl to normal standing after a few seconds and clears hit message
   reset = setTimeout(function(){
-    hitMessage = new Image();
-    girl = new Image();
-    girl.onload = onImageLoad;
-    girl.src = '/images/chibi-girl.png';
+    game.hitMessage = new Image();
+    game.girl = new Image();
+    game.girl.onload = onImageLoad;
+    game.girl.src = '/images/chibi-girl.png';
     animate()
   }, 2500);
 };
+
+//main animation function
+var animate = function(){
+  game.context.clearRect(0, 0, game.element.width, game.element.height);
+  game.context.drawImage(game.frames[game.frame], 0, 595);
+  game.context.drawImage(game.background, 0, 0);
+  game.context.drawImage(game.girl, 530, 250);
+  game.context.drawImage(game.arrows[0], 50, 10);
+  game.context.drawImage(game.arrows[1], 530, 10);
+  game.context.drawImage(game.arrows[2], 1060, 10);
+  game.context.drawImage(game.arrows[7], 50, 400);
+  game.context.drawImage(game.arrows[3], 1060, 400);
+  game.context.drawImage(game.arrows[6], 50, 700);
+  game.context.drawImage(game.arrows[5], 530, 700);
+  game.context.drawImage(game.arrows[4], 1060, 700);
+  //decides where to put hit message if there is one
+  if(game.hitMessage.src != '#'){
+    switch(game.direction){
+      case 'up_left':
+        game.context.drawImage(game.hitMessage, 50, 10);
+        break;
+      case 'up_right':
+        game.context.drawImage(game.hitMessage, 1060, 10);
+        break;
+      case 'left':
+        game.context.drawImage(game.hitMessage, 50, 400);
+        break;
+      case 'right':
+        game.context.drawImage(game.hitMessage, 1060, 400);
+        break;
+      case 'back_left':
+        game.context.drawImage(game.hitMessage, 50, 700);
+        break;
+      case 'back_right':
+        game.context.drawImage(game.hitMessage, 1060, 700);
+        break;
+      default:
+        //do nothing
+        break;
+    }
+  }
+  game.frame = ((game.frame + 1) % game.floors.length);
+};
+
+//register key press event
+$(document).ready(function(){
+  $(document).keypress(function(event){
+    keyPressEvent(event);
+  });
+});
