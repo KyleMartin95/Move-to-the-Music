e0fc8d3856a8dbb22456c6f4ea41e7ea0a341f94
KyleMartin95 <kylemartin95@gmail.com>
2017-04-21 18:19:30 -0400

    added mongodb connection

--- config/connections.js
+++ config/connections.js
@@ -56,14 +56,14 @@ module.exports.connections = {
   * Run: npm install sails-mongo                                             *
   *                                                                          *
   ***************************************************************************/
-  // someMongodbServer: {
-  //   adapter: 'sails-mongo',
-  //   host: 'localhost',
-  //   port: 27017,
-  //   user: 'username', //optional
-  //   password: 'password', //optional
-  //   database: 'your_mongo_db_name_here' //optional
-  // },
+  mtmMongodb: {
+     adapter: 'sails-mongo',
+     host: '127.0.0.1',
+     port: 27017,
+     user: 'MTM_user', //optional
+     password: 'KA18o4PPtA8WqU35', //optional
+     database: 'Move-to-the-Music' //optional
+  },
 
   /***************************************************************************
   *                                                                          *
