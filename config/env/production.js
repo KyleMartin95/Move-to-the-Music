f256434f4fe1d22d0b699ac1c4106c2a4814b6c8
KyleMartin95 <kylemartin95@gmail.com>
2017-04-21 15:11:00 -0400

    added stuff for production

--- config/env/production.js
+++ config/env/production.js
@@ -25,7 +25,8 @@ module.exports = {
    * Set the port in the production environment to 80                        *
    ***************************************************************************/
 
-  // port: 80,
+   port: process.env.OPENSHIFT_NODEJS_PORT,
+   host: process.env.OPENSHIFT_NODEJS_IP
 
   /***************************************************************************
    * Set the log level in production environment to "silent"                 *
